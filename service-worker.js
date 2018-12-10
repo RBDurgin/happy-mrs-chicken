e49b6cadc355d9eb8c72398d34dc874e4dcf5c46
Robert Durgin <>
2018-12-10 10:28:15 -0500

    Added Service Worker.

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+// Listen for install event, set callback
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll([
+          "/images/icons-192.png",
+          "/images/icons-256.png",
+          "/styles.css", 
+          "/mrs-chicken.js", 
+          "/index.html"]);
+    })
+  );
+  console.log("Happy Mrs. Chicken Registered!");
+});
+
+self.addEventListener("activate", function(event) {
+  console.log("Happy Mrs. Chicken Service Worker Activated!");
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
