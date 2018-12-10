88cff9daa994bb44a65bd5857aa4f48c7f63f6e2
Robert Durgin <>
2018-12-10 13:00:50 -0500

    Update cache strategy.

--- service-worker.js
+++ service-worker.js
@@ -3,15 +3,15 @@ const CACHE_NAME = "HAPPY_CHICKEN";
 self.addEventListener("install", function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll([        
-        '/happy-mrs-chicken',
-        '/happy-mrs-chicken',
-        './images/icons-192.png',
-        './images/icons-256.png',
-        './styles.css',
-        './mrs-chicken.js',
-        './index.html',
-        './404.html'
+      return cache.addAll([
+        "/happy-mrs-chicken",
+        "/happy-mrs-chicken/",
+        "./images/icons-192.png",
+        "./images/icons-256.png",
+        "./styles.css",
+        "./mrs-chicken.js",
+        "./index.html",
+        "./404.html"
       ]);
     })
   );
@@ -27,17 +27,25 @@ self.addEventListener("fetch", function(event) {
   event.respondWith(
     caches
       .match(event.request)
-      .then(function(response) {
-        return fetch(event.request) || response;
-      })
       .then(response => {
-        if (response.status === 404) {
-          return caches.match("404.html");
-        }
-        return caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request.url, response.clone());
+        if (response) {
+          console.log("Found ", event.request.url, " in cache");
           return response;
+        }
+        console.log("Network request for ", event.request.url);
+        return fetch(event.request).then(response => {
+          if (response.status === 404) {
+            return caches.match("pages/404.html");
+          }
+          return caches.open(staticCacheName).then(cache => {
+            cache.put(event.request.url, response.clone());
+            return response;
+          });
         });
+      })
+      .catch(error => {
+        console.log("Error, ", error);
+        return caches.match("pages/offline.html");
       })
   );
 });
