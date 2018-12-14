80e748c7d4a52d85c58a4a139b3220e65de8e0f9
Robert Durgin <>
2018-12-13 21:38:27 -0500

    Added color + reset.

--- mrs-chicken.js
+++ mrs-chicken.js
@@ -1,55 +1,105 @@
-var chicken = document.getElementsByClassName("chicken")[0];
-var container = document.getElementsByClassName("game-board")[0];
-var originalEgg = document.getElementsByClassName("egg-original")[0];
-var scoreSpan = document.getElementsByClassName('score-value')[0];
+const STEP = 25;
+const LONG_TOUCH_DURATION = 500;
+const COLORS = ['yellow', 'red', 'blue', 'brown', 'purple', 'green', 'pink'];
 
-var scale = 1.0;
-chicken.style.top = "250px";
-chicken.style.left = "250px";
-originalEgg.style.top = "250px";
-originalEgg.style.left = "250px";
+const chicken = document.getElementsByClassName('chicken')[0];
+const container = document.getElementsByClassName('game-board')[0];
+const originalEgg = document.getElementsByClassName('egg-original')[0];
+const scoreSpan = document.getElementsByClassName('score-value')[0];
+const colorBtn = document.getElementsByClassName('color')[0];
+const resetBtn = document.getElementsByClassName('reset')[0];
 
-var colors = ["yellow", "red", "blue", "brown", "purple", "green", "pink"];
-var colorIdx = 0;
+let scale = 1.0;
+chicken.style.top = '250px';
+chicken.style.left = '250px';
+originalEgg.style.top = '250px';
+originalEgg.style.left = '250px';
 
-var active = false;
-var currentX;
-var currentY;
-var initialX;
-var initialY;
-var xOffset = 0;
-var yOffset = 0;
-var step = 25;
-var score = 0;
+let colorIdx = 0;
 
-container.addEventListener("touchstart", dragStart, false);
-container.addEventListener("touchend", dragEnd, false);
-container.addEventListener("touchmove", drag, false);
-container.addEventListener("mousemove", drag, false);
-container.addEventListener("mousedown", dragStart, false);
-container.addEventListener("mouseup", dragEnd, false);
-container.addEventListener("dblclick", layEgg, false);
+let active = false;
+let currentX;
+let currentY;
+let initialX;
+let initialY;
+let xOffset = 0;
+let yOffset = 0;
+let score = 0;
+let touchTimer = null;
+
+function setTranslate(xPos, yPos, el) {
+  el.style.transform = `translate3d(${xPos}px, ${yPos}px, 0)`;
+}
+
+function longTouch(x, y) {
+  if (touchTimer) {
+    clearTimeout(touchTimer);
+    touchTimer = null;
+  }
+  container.dispatchEvent(
+    new CustomEvent('longtouch', {
+      x,
+      y,
+    }),
+  );
+}
+
+function dropEgg(x, y) {
+  const egg = document.getElementsByClassName('egg-original')[0];
+  const droppedEgg = egg.cloneNode();
+  droppedEgg.classList.remove('egg-original');
+
+  document.getElementsByClassName('game-board')[0].prepend(droppedEgg);
+  setTranslate(x, y, droppedEgg);
+  droppedEgg.style.transform += ' rotate(90deg)';
+  droppedEgg.style.display = 'block';
+  incrementScore(10);
+}
+
+function reset() {
+  chicken.style.top = '250px';
+  chicken.style.left = '250px';
+  chicken.style.width = '50px';
+  chicken.style.height = '50px';
+  chicken.style.transform = '';
+}
 
 function dragStart(e) {
-  if (e.type === "touchstart") {
-    initialX = e.touches[0].clientX - xOffset;
-    initialY = e.touches[0].clientY - yOffset;
+  if (e.target === resetBtn) {
+    e.preventDefault();
+    reset();
+  } else if (e.target === colorBtn) {
+    e.preventDefault();
+    changeColor();
   } else {
-    initialX = e.clientX - xOffset;
-    initialY = e.clientY - yOffset;
-  }
+    if (e.type === 'touchstart') {
+      initialX = e.touches[0].clientX - xOffset;
+      initialY = e.touches[0].clientY - yOffset;
+    } else {
+      initialX = e.clientX - xOffset;
+      initialY = e.clientY - yOffset;
+    }
 
-  if (e.target === chicken) {
-    active = true;
+    if (!touchTimer) {
+      touchTimer = setTimeout(longTouch, LONG_TOUCH_DURATION);
+    }
+
+    if (e.target === chicken) {
+      active = true;
+    }
   }
 }
 
 function drag(e) {
-  // e.preventDefault();
   if (active) {
     e.preventDefault();
 
-    if (e.type === "touchmove") {
+    if (touchTimer) {
+      clearTimeout(touchTimer);
+      touchTimer = null;
+    }
+
+    if (e.type === 'touchmove') {
       currentX = e.touches[0].clientX - initialX;
       currentY = e.touches[0].clientY - initialY;
     } else {
@@ -70,84 +120,76 @@ function dragEnd(e) {
 
   active = false;
 
-  var egg = document.getElementsByClassName("egg-original")[0];
-  console.log(egg);
-  var droppedEgg = egg.cloneNode();
-  droppedEgg.classList.remove("egg-original");
-  var x = currentX;
-  var y = currentY + 50;
-  console.log("evt.clientX: ", e.clientX, "evt.clientY:", e.clientY);
-  console.log("x:", x, " y:", y);
-  document.getElementsByClassName("game-board")[0].prepend(droppedEgg);
-  setTranslate(x, y, droppedEgg);
-  droppedEgg.style.transform += ' rotate(90deg)';
-  droppedEgg.style.display = "block";
-  score += 10;
+  if (touchTimer) {
+    clearTimeout(touchTimer);
+    isTouching = false;
+  }
+
+  const eggX = currentX;
+  const eggY = currentY + 50;
+  dropEgg(eggX, eggY);
+}
+
+function incrementScore(scoreDelta) {
+  score += scoreDelta;
   scoreSpan.innerHTML = score;
 }
 
-function setTranslate(xPos, yPos, el) {
-  el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
-}
-
 function changeColor() {
-  if (colorIdx < colors.length) {
-    colorIdx++;
+  if (colorIdx < COLORS.length) {
+    colorIdx += 1;
   } else {
     colorIdx = 0;
   }
 
-  chicken.style.backgroundColor = colors[colorIdx];
+  chicken.style.backgroundColor = COLORS[colorIdx];
 }
 
-function layEgg(evt) {
-}
+function layEgg(evt) {}
 
-function reset() {
-  chicken.style.top = "250px";
-  chicken.style.left = "250px";
-  chicken.style.width = "50px";
-  chicken.style.height = "50px";  
-}
+container.addEventListener('touchstart', dragStart, false);
+container.addEventListener('touchend', dragEnd, false);
+container.addEventListener('touchmove', drag, false);
+container.addEventListener('mousemove', drag, false);
+container.addEventListener('mousedown', dragStart, false);
+container.addEventListener('mouseup', dragEnd, false);
+container.addEventListener('dblclick', layEgg, false);
+container.addEventListener('longtouch', changeColor, false);
 
-window.addEventListener("keydown", function(evt) {
+window.addEventListener('keydown', (evt) => {
+  const left = parseInt(chicken.style.left);
+  const top = parseInt(chicken.style.top);
+
   if (evt.keyCode === 38) {
-    // up
-    console.log(chicken.style.top);
-    var top = parseInt(chicken.style.top);
-    console.log("top", top - step + "px");
-    chicken.style.top = top - step + "px";
+    // up.
+    chicken.style.top = `${top - STEP}px`;
   } else if (evt.keyCode === 39) {
-    // right
-    var left = parseInt(chicken.style.left);
-    chicken.style.left = left + step + "px";
-    chicken.style.transform = "scale(-1, 1)";
+    // right.
+    chicken.style.left = `${left + STEP}px`;
+    chicken.style.transform = 'scale(-1, 1)';
   } else if (evt.keyCode === 37) {
-    // left
-    var left = parseInt(chicken.style.left);
-    chicken.style.left = left - step + "px";
-    chicken.style.transform = "scale(1, 1)";
+    // left.
+    chicken.style.left = `${left - STEP}px`;
+    chicken.style.transform = 'scale(1, 1)';
   } else if (evt.keyCode === 40) {
-    // down
-    var top = parseInt(chicken.style.top);
-    chicken.style.top = top + step + "px";
+    // down.
+    chicken.style.top = `${top + STEP}px`;
   } else if (evt.keyCode === 82) {
     reset();
   } else if (evt.keyCode === 188) {
     scale -= 0.1;
     // chicken.transform = 'scale(' + scale + ', ' + scale + ')';
-    var width = parseInt(chicken.offsetWidth);
-    var height = parseInt(chicken.offsetHeight);
-    chicken.style.width = width - step + "px";
-    chicken.style.height = height - step + "px";
+    const width = parseInt(chicken.offsetWidth);
+    const height = parseInt(chicken.offsetHeight);
+    chicken.style.width = `${width - STEP}px`;
+    chicken.style.height = `${height - STEP}px`;
   } else if (evt.keyCode === 190) {
     scale += 0.1;
-
-    //chicken.transform = HJNNJ'scale(' + scale + ', ' + scale + ')';
-    var width = parseInt(chicken.offsetWidth);
-    var height = parseInt(chicken.offsetHeight);
-    chicken.style.width = width + 20 + "px";
-    chicken.style.height = height + 20 + "px";
+    // chicken.transform = HJNNJ'scale(' + scale + ', ' + scale + ')';
+    const width = parseInt(chicken.offsetWidth);
+    const height = parseInt(chicken.offsetHeight);
+    chicken.style.width = `${width + 20}px`;
+    chicken.style.height = `${height + 20}px`;
   } else if (evt.keyCode === 32) {
     // space.
     changeColor();
